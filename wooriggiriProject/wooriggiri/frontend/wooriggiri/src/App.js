0426aee1d97a8a455c67f76df4f7c5b20dfee047
UitaeRyu <ryan-0826@naver.com>
2024-05-24 14:11:59 +0900

    초기 프로젝트 설정

---
+++ wooriggiriProject/wooriggiri/frontend/wooriggiri/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>wooriggiri</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ wooriggiriProject/wooriggiri/src/test/java/com/wooriggiri/wooriggiri/WooriggiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooriggiri.wooriggiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriggiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
