44864fe5e9016f97d7ae0973f6471a5d69af8000
heegg02 <h111kyung9@gmail.com>
2024-06-04 21:04:22 +0900

    업데이트 20240604

--- wooriggiriProject/wooriggiri/frontend/wooriggiri/src/components/LoginForm.js
+++ wooriggiriProject/wooriggiri/frontend/wooriggiri/src/components/LoginForm.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styles from './styles/loginForm.module.css'
 
-function LoginForm({ signUpClick }) {
+function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+    
+        try {
+          const response = await axios.post('http://localhost:8080/login', {
+            email,
+            password,
+          }, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+    
+          if (response.status === 200) {
+            
+            window.location.href = '/';
+          }
+        } catch (err) {
+          setError('로그인 실패. 사용자 이름과 비밀번호를 확인하세요.');
+          console.log(error);
+        }
+      };
+
     return (
         <>
             <h3>로그인</h3>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <div className={styles.input_group}>
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="이메일" id="email" name="email" required />
+                    <input
+                        type="email"
+                        placeholder="이메일"
+                        id="email"
+                        name="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className={styles.input_group}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="비밀번호" id="password" name="password" required />
+                    <input
+                        type="password"
+                        placeholder="비밀번호"
+                        id="password"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
                 <button className={styles.btn} type="submit">로그인</button>
             </form>
---
+++ wooriggiriProject/wooriggiri/src/main/java/com/wooriggiri/api/dto/SignupRequest.java
@@ -0,0 +1,14 @@
+package com.wooriggiri.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+    private String email;
+    private String password;
+    private String username;
+}
