d84bafb06073aa7f9e81f156fe229d43e25ddeda
heegg02 <h111kyung9@gmail.com>
2024-06-04 11:49:28 +0900

    업데이트 20240604

--- wooriggiriProject/wooriggiri/src/main/java/com/wooriggiri/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wooriggiri.repository;
-
-import com.wooriggiri.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
